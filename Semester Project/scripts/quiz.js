c7a547b05faf89024796623026fd211cc48ee0c9
belladianamiller <isabelladianamiller@gmail.com>
2024-10-23 13:51:19 -0400

    Update quiz.js

--- Semester Project/scripts/quiz.js
+++ Semester Project/scripts/quiz.js
@@ -1,4 +1,4 @@
-const quizData = {
+const quizData = [
     {
         question: "How would you describe your skin type?",
         answers: [
@@ -9,6 +9,45 @@ const quizData = {
         ],
     },
     {
-        
-    }
-}
+        question: "Which skin concern is most significant to you?",
+        answers: [
+            {text: "Acne or acne marks"},
+            {text: "Anti-aging"},
+            {text: "Dryness or flaky skin"},
+            {text: "Skin texture"},
+            {text: "Dullness"},
+        ],
+    },
+    {
+        question: "Is your skin sensitive?",
+        answers: [
+            {text: "Skin is irritated easily"},
+            {text: "Skin is rarely irritated"},
+            {text: "Unsure"},
+        ],
+    },
+    {
+        question: "How would you describe your acne?",
+        answers: [
+            {text: "No acne"},
+            {text: "Mild"},
+            {text: "Moderate"},
+            {text: "Severe"},
+        ],
+    },
+    {
+        question: "What is your age group?",
+        answers: [
+            {text: "Under 12"},
+            {text: "Age 13-17"},
+            {text: "Age 18-65"},
+            {text: "Over 65"},
+        ],
+    },
+    {
+        question: "What skincare products do you currently use? (Select all that apply)",
+        answers: [
+            {text: "cleanser"},
+        ],
+    },
+];
